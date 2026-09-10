e155ad52f3c4c218f8f1ad2f8916efcc06447be7
Project Crew <user@domain.com>
2026-09-09 22:23:30 -0400

    png borders

--- media/tex2pdf.sh
+++ media/tex2pdf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # generate pdf and png from the TeX file
 BASE=$(basename -s .tex ${1})
-pdflatex ${1}
-pdftoppm -png -r 300 ${BASE}.pdf > ${BASE}.png
+latex2png -d 900 ${1}
+mogrify -resize 25% -bordercolor white -border 30 ${BASE}.png
