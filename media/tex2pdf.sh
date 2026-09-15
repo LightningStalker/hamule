fbe78e60b4efee79d2a2ee46a34a69bf3817fe74
Project Crew <user@domain.com>
2026-09-14 23:00:52 -0400

    up script

--- media/tex2pdf.sh
+++ media/tex2pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # generate pdf and png from the TeX file
 BASE=$(basename -s .tex ${1})
-rm -f *.log *.gz *.aux *.dvi
+rm -f *.log *.gz *.aux *.dvi *.pdf
 latex2png -m -d 900 ${1}
 mogrify -quiet -resize 25% -bordercolor white -border 20 ${BASE}.png
